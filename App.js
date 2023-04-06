cfa0e8ff5a9d918a4ac07804a78d24519c4f50c2
Miecevit <mert.iecevit@gmail.com>
2023-04-06 13:20:21 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
 import React from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Screens/Login';
 import Signup from './Screens/Signup';
+import Home from './Screens/Home';
+
+import {firebase} from './config.js';
 
 
 const Stack = createStackNavigator();
 
+const AuthContext = React.createContext();
+
 export default function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        setUser(user);
+      }else{
+        setUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authContextValue = useMemo(
+    () => ({
+
+      signIn: async (email, password) => {
+        try{
+          await firebase.auth.signInWithEmailAndPassword(email, password);
+        }catch (error){
+          console.log(error.message);
+        }
+      },
+
+      signOut: async () => {
+        try{
+          await firebase.auth().signOut();
+        }catch(error){
+          console.log(error.message);
+        }
+      },
+    }), []);
+
+
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
       </Stack.Navigator>
