dec0dbb69808d898e9d6640824489511c1d6df09
Miecevit <mert.iecevit@gmail.com>
2023-04-06 11:26:18 +0300

    Add files via upload

---
+++ Screens/Home.js
@@ -0,0 +1,129 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = () => {
+
+  return (
+
+    <SafeAreaView style={ {flex: 1} }>
+
+      <View style={styles.textHeadingContainer}>
+        <Text style={styles.textHeading}>100KY - ToDo</Text>
+      </View>
+
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add New ToDo"
+          placeholderTextColor= "#cceeff"
+          value={addData}
+          onChangeText={(heading) => setAddData(heading)}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+          />
+        <TouchableOpacity style={styles.button} onPress={addTodo}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={todoList}
+        numColumns={1}
+        renderItem= {( {item} ) => (
+          <View style={styles.container}>
+            <Text style={styles.itemHeading}> {item.heading} </Text>
+          </View>
+        )}
+        />
+    </SafeAreaView>
+  );
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+    backgroundColor: '#e5e5e5',
+    padding: 15,
+    borderRadius: 15,
+    margin: 5,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textHeadingContainer: {
+    paddingVertical: 20,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  textHeading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  innerContainer: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginLeft: 45,
+  },
+  itemHeading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginRight: 22,
+  },
+  formContainer: {
+    flexDirection: 'row',
+    height: 80,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 40,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
+  },
+  button: {
+    height: 47,
+    borderRadius: 5,
+    backgroundColor: '#788eec',
+    width: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+
+  todoIcon: {
+    marginTop: 5,
+    fontSize: 20,
+    marginLeft: 14,
+  },
+   buttonSignOut: {
+    marginTop: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 10,
+    backgroundColor: '#ff2e2e',
+  },
+  buttonSignOutText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
